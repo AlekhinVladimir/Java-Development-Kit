0a60756354207a074c959550cebed11c3bfb73b3
AlekhinVladimir <119034591+AlekhinVladimir@users.noreply.github.com>
2023-12-29 15:54:50 +0400

    5 seminar and homework

---
+++ 5/Homework5/PhilosopherTask.java
@@ -0,0 +1,15 @@
+package Homework5;
+
+public class PhilosopherTask {
+    static int philosophersQuantity = 5;
+    Meal meal = new Meal();
+
+    public PhilosopherTask() {
+        for (int i = 0; i < philosophersQuantity; i++) {
+            Thread thread = new Thread(new Philosopher(meal));
+            thread.setName("Philosopher-" + (i + 1));
+            thread.start();
+        }
+    }
+
+}
